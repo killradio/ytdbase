780bfba42ea92395ea051fca490fdc79b2788d7f
KiriX <KiriX@e70539a1-e9aa-4707-9e48-a9b25a7622e0>
2009-07-21 06:56:46 +0000

    Реорганизация структуры SVN репозитория
    git-svn-id: http://svn2.assembla.com/svn/ytdbase@169 e70539a1-e9aa-4707-9e48-a9b25a7622e0

--- WOtLK/R46_WOtLK/Updates/472_corepatch_characters_7265_to_7298.sql
+++
@@ -1,5 +0,0 @@
-# 7267
-ALTER TABLE character_db_version CHANGE COLUMN required_7255_01_characters_characters required_7267_01_characters_auctionhouse bit;
-
-ALTER TABLE auctionhouse
-  DROP COLUMN location;
