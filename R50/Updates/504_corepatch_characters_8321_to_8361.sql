fefa69fa05d926318b8adaa90ef310c8428894df
ZiroX <ZiroX@e70539a1-e9aa-4707-9e48-a9b25a7622e0>
2009-08-13 10:23:18 +0000

    Обновление 504_(8361) + все исправления с форума и правки по багрепортам (см. раздел "принятые патчи") + скрипты квестов + заселение + правка "creature_template" и "gameobject_template" из WDB (всего обработанно 79 кешей) + правка статов итемов из WDB (Total: 29781/33382) - на этом закончили разбор кешей от 3.1.3 - всем обязательно чистить кеш игры!
    Update 504_ (8361) 
    + All fixes from the forum and edits on Bug reports (see "adopted patches") 
    + Scripts quests 
    + Settlement 
    + Edit "creature_template" and "gameobject_template" of WDB (all processed keshey 79) 
    + Edit article items of WDB (Total: 29781/33382) - to finish this review of keshey 3.1.3 
    - All sure to clean the cache of the game! 
    
    
    git-svn-id: http://svn2.assembla.com/svn/ytdbase@176 e70539a1-e9aa-4707-9e48-a9b25a7622e0

---
+++ R50/Updates/504_corepatch_characters_8321_to_8361.sql
@@ -0,0 +1,27 @@
+ALTER TABLE character_db_version CHANGE COLUMN required_8104_01_characters required_8339_01_characters_characters bit;
+
+ALTER TABLE characters DROP COLUMN bgid;
+ALTER TABLE characters DROP COLUMN bgteam;
+ALTER TABLE characters DROP COLUMN bgmap;
+ALTER TABLE characters DROP COLUMN bgx;
+ALTER TABLE characters DROP COLUMN bgy;
+ALTER TABLE characters DROP COLUMN bgz;
+ALTER TABLE characters DROP COLUMN bgo;
+
+ALTER TABLE character_db_version CHANGE COLUMN required_8339_01_characters_characters required_8339_02_characters_character_battleground_data bit;
+
+DROP TABLE IF EXISTS `character_battleground_data`;
+CREATE TABLE `character_battleground_data` (
+  `guid` int(11) unsigned NOT NULL default '0' COMMENT 'Global Unique Identifier',
+  `instance_id` int(11) unsigned NOT NULL default '0',
+  `team` int(11) unsigned NOT NULL default '0',
+  `join_x` float NOT NULL default '0',
+  `join_y` float NOT NULL default '0',
+  `join_z` float NOT NULL default '0',
+  `join_o` float NOT NULL default '0',
+  `join_map` int(11) NOT NULL default '0',
+  `taxi_start` int(11) NOT NULL default '0',
+  `taxi_end` int(11) NOT NULL default '0',
+  `mount_spell` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='Player System';
