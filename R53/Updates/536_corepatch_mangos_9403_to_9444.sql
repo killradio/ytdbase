2afe5b0a8cd48060ed89e589603999a75cb6a835
ZirOX <ZirOX@e70539a1-e9aa-4707-9e48-a9b25a7622e0>
2010-02-25 07:51:25 +0000

    git-svn-id: http://svn2.assembla.com/svn/ytdbase@282 e70539a1-e9aa-4707-9e48-a9b25a7622e0

---
+++ R53/Updates/536_corepatch_mangos_9403_to_9444.sql
@@ -0,0 +1,12 @@
+
+
+# SD2_1601
+UPDATE creature_template SET ScriptName='npc_hulking_helboar' WHERE entry=16880;
+UPDATE creature_template SET ScriptName='npc_morbent_fel' WHERE entry=1200;
+UPDATE creature_template SET ScriptName='spell_dummy_npc' WHERE entry IN (16880,1200,26616,26643,16518,25793,25758,25752,25792,25753,26421);
+UPDATE creature_template SET ScriptName='spell_dummy_npc' WHERE entry IN (26841,27808,27122);
+UPDATE creature_template SET ScriptName='spell_dummy_npc' WHERE entry=28068;
+UPDATE creature_template SET ScriptName='spell_dummy_npc' WHERE entry IN (12298,12296);
+UPDATE creature_template SET ScriptName='spell_dummy_npc' WHERE entry=24918;
+UPDATE creature_template SET ScriptName='npc_dalaran_guardian_mage' WHERE entry IN (29255, 29254);
+UPDATE creature_template SET ScriptName='npc_dame_evniki_kapsalis' WHERE entry=34885;
