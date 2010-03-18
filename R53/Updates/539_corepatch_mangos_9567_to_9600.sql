99d33fbd20cd3e1eb5ee5f03c95e72054ea8c477
ZirOX <ZirOX@e70539a1-e9aa-4707-9e48-a9b25a7622e0>
2010-03-18 08:13:28 +0000

    git-svn-id: http://svn2.assembla.com/svn/ytdbase@299 e70539a1-e9aa-4707-9e48-a9b25a7622e0

---
+++ R53/Updates/539_corepatch_mangos_9567_to_9600.sql
@@ -0,0 +1,11 @@
+ALTER TABLE db_version CHANGE COLUMN required_9539_01_mangos_spell_bonus_data required_9589_01_mangos_creature_template bit;
+
+ALTER TABLE creature_template ADD COLUMN speed_run float NOT NULL default '1.14286' COMMENT 'Result of 8.0/7.0, most common value' AFTER speed;
+ALTER TABLE creature_template CHANGE COLUMN speed speed_walk float NOT NULL default '1' COMMENT 'Result of 2.5/2.5, most common value';
+
+ALTER TABLE db_version CHANGE COLUMN required_9589_01_mangos_creature_template required_9590_01_mangos_db_script_string bit;
+
+ALTER TABLE `db_script_string` CHANGE `entry` `entry` INT( 11 ) UNSIGNED NOT NULL DEFAULT '0';
+
+
+# SD2_1643
