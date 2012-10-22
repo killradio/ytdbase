05885d231fc741fcb9316ebad84bda871ce5b156
ZirOX <ZirOX@e70539a1-e9aa-4707-9e48-a9b25a7622e0>
2012-10-22 17:53:24 +0000

    Сортировка данных
    git-svn-id: http://svn2.assembla.com/svn/ytdbase@412 e70539a1-e9aa-4707-9e48-a9b25a7622e0

--- R53/Updates/540_corepatch_mangos_9600_to_9613.sql
+++
@@ -1,7 +0,0 @@
-
-
-# SD2_1653
-UPDATE creature_template SET ScriptName='npc_corporal_keeshan' WHERE entry=349;
-UPDATE creature_template SET ScriptName='boss_netherspite' WHERE entry=15689;
-UPDATE instance_template SET script='instance_uldaman' WHERE map=70;
-UPDATE gameobject_template SET ScriptName='go_altar_of_keepers' WHERE entry=130511;
